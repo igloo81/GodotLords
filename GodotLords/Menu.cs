672a5b993ab7d3dc652501ee16e83963b8b87639
jasperdekoning <jasper.dekoning@gmail.com>
2025-03-10 14:56:04 +0100

    Add menu

--- GodotLords/Menu.cs
+++ GodotLords/Menu.cs
@@ -3,13 +3,15 @@ using System;
 
 public partial class Menu : Control
 {
-	// Called when the node enters the scene tree for the first time.
+	[Export] public Color HoverColor = new Color(0.2f, 0.2f, 0.2f); // Lighter color on hover
+
 	public override void _Ready()
 	{
 		var startButton = GetNode<Button>("Menu/Start");
 		startButton.Pressed += OnStartPressed;
 
 		var quitButton = GetNode<Button>("Menu/Quit");
+		quitButton.Pressed += OnQuitPressed;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,4 +23,9 @@ public partial class Menu : Control
 	{
 		GetTree().ChangeSceneToFile("res://node_2d.tscn");
 	}
+
+	public void OnQuitPressed()
+	{
+		GetTree().Quit();
+	}	
 }
