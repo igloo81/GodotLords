2f25f6f7584f97ccdbfc236c64954d4dc95e0aef
jasperdekoning <jasper.dekoning@gmail.com>
2025-03-21 11:37:48 +0100

    Allow selection of tile

--- GodotLords/MapView/UnitsMapLayer.cs
+++ GodotLords/MapView/UnitsMapLayer.cs
@@ -3,9 +3,18 @@ using System.Collections.Generic;
 
 public partial class UnitsMapLayer : TileMapLayer
 {
+    private Vector2I _selectedCell = new Vector2I(-1, -1);
+    private ColorRect _selectionRect;
+
     public override void _Ready()
     {
         this.SetCell(new Vector2I(10, 10), 0, new Vector2I(0, 0));
+
+        // Create a selection rectangle
+        _selectionRect = new ColorRect();
+        _selectionRect.Color = new Color(1, 1, 0, 0.3f); // Semi-transparent yellow
+        AddChild(_selectionRect);
+        _selectionRect.Visible = false;
     }
 
     // This function is called when you click on the screen.
@@ -27,6 +36,10 @@ public partial class UnitsMapLayer : TileMapLayer
             // Print the tile coordinates to the console
             GD.Print("Tile selected at: " + tileCoords);
 
+            // Update selection
+            _selectedCell = tileCoords;
+            UpdateSelectionRect();
+
             // Optionally, get the tile ID at that position
             //int tileId = GetCellSourceId(0, tileCoords);
             //GD.Print("Tile ID: " + tileId);
@@ -35,4 +48,21 @@ public partial class UnitsMapLayer : TileMapLayer
             // SetCell(0, tileCoords, 0, new Vector2I(0, 0));
         }
     }    
+
+    private void UpdateSelectionRect()
+    {
+        if (_selectedCell.X < 0 || _selectedCell.Y < 0)
+        {
+            _selectionRect.Visible = false;
+            return;
+        }
+
+        // Get the tile size
+        var tileSize = TileSet.TileSize;
+        
+        // Position the selection rectangle
+        _selectionRect.Position = MapToLocal(_selectedCell) - tileSize / 2;
+        _selectionRect.Size = tileSize;
+        _selectionRect.Visible = true;
+    }
 } 
