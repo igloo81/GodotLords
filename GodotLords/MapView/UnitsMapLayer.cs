107d8267a80ff27fcc010c145312efeefbebee83
jasperdekoning <jasper.dekoning@gmail.com>
2025-03-21 11:06:17 +0100

    Show unit

--- GodotLords/MapView/UnitsMapLayer.cs
+++ GodotLords/MapView/UnitsMapLayer.cs
@@ -1,12 +1,11 @@
 using Godot;
 using System.Collections.Generic;
 
-public partial class UnitsMapLayer : Node2D
+public partial class UnitsMapLayer : TileMapLayer
 {
 
     public override void _Ready()
     {
-    }
-
-    
+		this.SetCell(new Vector2I(10, 10), 0, new Vector2I(0, 0));
+    }   
 } 
