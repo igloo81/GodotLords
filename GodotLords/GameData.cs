b5fc669fb52bf6b7b49804549dd7882f8e74aaa5
jasperdekoning <jasper.dekoning@gmail.com>
2025-03-25 15:35:58 +0100

    Use shared GameData

--- GodotLords/GameData.cs
+++ GodotLords/GameData.cs
@@ -2,20 +2,9 @@ using System;
 using System.Collections.Generic;
 using Godot;
 
-public class MapData
+public class GameData
 {
-    public static Lazy<Map> GameMap = new Lazy<Map>(() => Map.FromImage(
-        "Resources/map.png", 2, 
-        new Dictionary<Color, TerrainType>{
-            { Color.FromHtml("#0053c9"), TerrainType.Water},
-            { Color.FromHtml("#217725"), TerrainType.Grass},
-            { Color.FromHtml("#555555"), TerrainType.Road},
-            { Color.FromHtml("#9a5600"), TerrainType.Hill},
-            { Color.FromHtml("#693500"), TerrainType.Mountain},
-            { Color.FromHtml("#005500"), TerrainType.Forest},
-        }
-        ));
-
+    public Map Map { get; set;}
     public List<UnitOnMap> UnitsOnMap { get; set; }
     public List<Unit> Units { get; set; }
     public List<City> Cities { get; set; }
