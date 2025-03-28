2d0fe11e1913ad32530f33ce412df461a94b4518
jasperdekoning <jasper.dekoning@gmail.com>
2025-03-28 15:00:58 +0100

    Fix location of scripts

--- GodotLords/MapView/UnitsMapLayer.cs
+++ GodotLords/MapView/UnitsMapLayer.cs
@@ -1,69 +1,11 @@
 using Godot;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using GodotLords.Engine;
 
-namespace GodotLords.MapView;
-
-public partial class UnitsMapLayer : TileMapLayer, IGameUpdateHandler    // todo this should be a set of node2ds
+public partial class UnitsMapLayer : TileMapLayer
 {
-    private Vector2I selectedCell = new Vector2I(-1, -1);
-    private ColorRect selectionRectangle;
-    private GameData gameData;
-    private PackedScene armyScene;
-
-    private Dictionary<Vector2I, Node2D> unitsOnScreen;
-
     public override void _Ready()
     {
-        this.unitsOnScreen = new Dictionary<Vector2I, Node2D>();
-        this.gameData = ((MapNode)GetParent()).GameData;
-        this.armyScene = (PackedScene)ResourceLoader.Load("res://ArmyScene.tscn");
-
-        foreach (var unitOnMap in gameData.UnitsOnMap)  // todo accessing these objects async...
-            ShowArmyOnMap(unitOnMap.Key, gameData.GetUnits(unitOnMap.Value).ToArray()); // todo use gameCommands instead?
-
-        InitializeSelectionRectangle();
-    }
-
-    private void InitializeSelectionRectangle()
-    {        
-        selectionRectangle = new ColorRect();
-        selectionRectangle.Color = new Color(1, 1, 0, 0.3f); // todo move the selection rectangle outside of this class
-        AddChild(selectionRectangle);
-        selectionRectangle.Visible = false;
-    }
-    
-	public void HandleUpdate(GodotLords.Engine.GameUpdate.IGameUpdate gameUpdate)
-	{
-        switch (gameUpdate)
-        {
-            case Engine.GameUpdate.MoveArmy move:
-                RemoveArmyOnMap(move.From);
-                if (move.UnitsLeft.Length > 0)
-                    ShowArmyOnMap(move.From, move.UnitsMoved);
-                ShowArmyOnMap(move.To, move.UnitsMoved);
-                break;
-        }
-	}
-
-    private void RemoveArmyOnMap(Vector2I position)
-    {
-        if (unitsOnScreen.TryGetValue(position, out var army))
-            RemoveChild(army);
-    }
-
-    private void ShowArmyOnMap(Vector2I position, Unit[] units)
-    {
-        var tileSize = 32; // todo share!
-
-        var newArmyScene = armyScene.Instantiate<ArmyScene>();
-        newArmyScene.Units = units;
-        newArmyScene.Position = position * tileSize + new Vector2I(tileSize / 2, tileSize / 2);
-
-        unitsOnScreen[position] = newArmyScene;
-        AddChild(newArmyScene);
+        this.SetCell(new Vector2I(10, 10), 0, new Vector2I(0, 0));
     }
 
     // This function is called when you click on the screen.
@@ -85,10 +27,6 @@ public partial class UnitsMapLayer : TileMapLayer, IGameUpdateHandler    // todo
             // Print the tile coordinates to the console
             GD.Print("Tile selected at: " + tileCoords);
 
-            // Update selection
-            selectedCell = tileCoords;
-            UpdateSelectionRect();
-
             // Optionally, get the tile ID at that position
             //int tileId = GetCellSourceId(0, tileCoords);
             //GD.Print("Tile ID: " + tileId);
@@ -97,23 +35,4 @@ public partial class UnitsMapLayer : TileMapLayer, IGameUpdateHandler    // todo
             // SetCell(0, tileCoords, 0, new Vector2I(0, 0));
         }
     }    
-
-    private void UpdateSelectionRect()
-    {
-        if (selectedCell.X < 0 || selectedCell.Y < 0)
-        {
-            selectionRectangle.Visible = false;
-            return;
-        }
-
-        // Get the tile size
-        var tileSize = TileSet.TileSize;
-        
-        // Position the selection rectangle
-        selectionRectangle.Position = MapToLocal(selectedCell) - tileSize / 2;
-        selectionRectangle.Size = tileSize;
-        selectionRectangle.Visible = true;
-
-        RemoveArmyOnMap(selectedCell);
-    }
 } 
