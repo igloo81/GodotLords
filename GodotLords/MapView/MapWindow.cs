62de6be82e2499abcb201718710cd312206cacdd
Igloo <jasper.dekoning@gmail.com>
2025-03-28 23:43:14 +0100

    Couple windows more dynamicaly

--- GodotLords/MapView/MapWindow.cs
+++ GodotLords/MapView/MapWindow.cs
@@ -5,8 +5,18 @@ namespace GodotLords.MapView;
 
 public partial class MapWindow : Control, IGameUpdateHandler
 {
-    public GameData GetGameData() =>  
-        ((GameWindow.GameWindow)GetParent()).GameData;
+    public GameData GetGameData()
+	{
+		var parent = GetParent();
+		while (parent != null && !(parent is GameWindow.GameWindow))
+			parent = parent.GetParent();
+		return ((GameWindow.GameWindow)parent).GameData;
+	}
+
+	public override void _Ready()
+	{
+		GetGameData().SomethingHappened += HandleUpdate;
+	}
 
 	public void HandleUpdate(GodotLords.Engine.GameUpdate.IGameUpdate gameUpdate)
 	{
@@ -15,5 +25,10 @@ public partial class MapWindow : Control, IGameUpdateHandler
 			if (child is IGameUpdateHandler childAsEventHandler)
 				childAsEventHandler.HandleUpdate(gameUpdate);
 		}
+	}
+
+	public override void _ExitTree()
+	{
+		GetGameData().SomethingHappened -= HandleUpdate;	// todo here or in a constructor?
 	}
 }
